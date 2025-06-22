7a9aa7a5be71098868ad704a8773484ae2485260
KelvinClassic <emekaamuzie@gmail.com>
2025-06-22 06:10:27 +0100

    Added bronze folder containing bronze DDL and Stored Procedure. Included Schema/Layers Creation SQL file

---
+++ create_schema.sql
@@ -0,0 +1,15 @@
+/*
+==============================================================================
+Script Purpose: 
+	Creates the schemas for the different layers ('bronze', 'silver' and 'gold') of the data warehouse
+==============================================================================
+*/
+
+-- CREATE GOLD LAYER
+CREATE SCHEMA gold;
+
+-- CREATE SILVER LAYER 
+CREATE SCHEMA silver;
+
+-- CREATE BRONZE LAYER
+CREATE SCHEMA bronze;
